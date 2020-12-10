77ab12ba5551d4cfb1402ecbf2ce4ab084fbf748
Asan9206 <60429656+Asan9206@users.noreply.github.com>
2020-12-10 15:48:29 +0800

    Update jd_pubg.js

--- jd_pubg.js
+++ jd_pubg.js
@@ -9,11 +9,14 @@ PUBG ,运行时间会比较久,Surge请加大timeout时间
 [task_local]
 #PUBG
 10 0 * * * https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_pubg.js, tag=PUBG, enabled=true
+
 ================Loon==============
 [Script]
 cron "10 0 * * *" script-path=https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_pubg.js,tag=PUBG
+
 ===============Surge=================
 PUBG = type=cron,cronexp="10 0 * * *",wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_pubg.js
+
 ============小火箭=========
 PUBG = type=cron,script-path=https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_pubg.js, cronexpr="10 0 * * *", timeout=200, enable=true
  */
@@ -205,7 +208,7 @@ function taskList(get=1) {
           if (safeGet(data)) {
             data = JSON.parse(data);
             let vo = data.data[0]
-            console.log(`您的${$.name}好友助力码为：${vo.shareId}`)
+            if (vo.shareId) console.log(`您的${$.name}好友助力码为：${vo.shareId}`)
             for (let i = 0; i< vo.venueList.length;++i){
               let venue = vo.venueList[i]
               if(venue.venueStatus === 1) {
@@ -231,7 +234,7 @@ function taskList(get=1) {
             }
             for (let i = 0; i< vo.shopList.length;++i){
               let shop = vo.shopList[i]
-              if(shop.shopStatus === 0) {
+              if(shop.shopStatus === 0 || shop.shopStatus === 1) {
                 console.log(`【关注店铺】去关注店铺 ${shop.shopName}`)
                 await doTask(`starId=PUBG&type=shop&id=${shop.shopId}&status=1`)
                 await $.wait(10000)
