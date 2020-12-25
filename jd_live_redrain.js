a09d48ad3c0dd07211d5037dfa66d7740e4c5ed3
Asan9206 <60429656+Asan9206@users.noreply.github.com>
2020-12-26 00:26:58 +0800

    Update jd_live_redrain.js

--- jd_live_redrain.js
+++ jd_live_redrain.js
@@ -1,7 +1,7 @@
 /*
 直播红包雨
 每天0,9,11,13,15,17,19,20,21,23可领，每日上限未知
-活动时间：2020-12-7 到 2020-12-12
+活动时间：2020-12-14 到 2020-12-31
 更新地址：https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_live_redrain.js
 已支持IOS双京东账号, Node.js支持N个京东账号
 脚本兼容: QuantumultX, Surge, Loon, 小火箭，JSBox, Node.js
@@ -9,21 +9,34 @@
 [task_local]
 #直播红包雨
 0 0,9,11,13,15,17,19,20,21,23 * * * https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_live_redrain.js, tag=直播红包雨, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jd_redPacket.png, enabled=true
+
 ================Loon==============
 [Script]
 cron "0 0,9,11,13,15,17,19,20,21,23 * * *" script-path=https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_live_redrain.js, tag=直播红包雨
+
 ===============Surge=================
 直播红包雨 = type=cron,cronexp="0 0,9,11,13,15,17,19,20,21,23 * * *",wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_live_redrain.js
+
 ============小火箭=========
 直播红包雨 = type=cron,script-path=https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_live_redrain.js, cronexpr="0 0,9,11,13,15,17,19,20,21,23 * * *", timeout=200, enable=true
  */
 const $ = new Env('直播红包雨');
-
+let ids = {
+  '0': 'RRA3S6TRRbnNNuGN43oHMA5okbcXmRY',
+  '9': 'RRA3vyGH4MRwCJELDwV7p24mNAByiSk',
+  '11': 'RRAnabmRSnpzSSZicXUhSFGBvFXs5c',
+  '13': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
+  '15': 'RRA2CnovS9KVTTwBD9NV7o4kc3P8PTN',
+  '17': 'RRA2nFXT2oSQM3KaYX9uhBC1hBijDey',
+  '19': 'RRA3SQpuSAAJq1ckoPr4TXaxwbLG73k',
+  '20': 'RRA2cHV3KXqvHAZGboTTryr8JMYZd5j',
+  '21': 'RRA3SPs4XrDEXXwQjEFGrBLtMpjtkMV',
+  '23': 'RRA3dFHoZXGThSnctvtAf69dmVyEDfm',
+}
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
-const randomCount = $.isNode() ? 20 : 5;
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
 if ($.isNode()) {
@@ -32,6 +45,24 @@ if ($.isNode()) {
   })
   if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => {
   };
+  process.env.TZ = "Asia/Shanghai";
+  Date.prototype.TimeZone = new Map([
+    ['Asia/Shanghai',+8],
+  ])
+  Date.prototype.zoneDate = function(){
+    if(process.env.TZ === undefined){
+      return new Date();
+    }else{
+      for (let item of this.TimeZone.entries()) {
+        if(item[0] === process.env.TZ){
+          let d = new Date();
+          d.setHours(d.getHours()+item[1]);
+          return d;
+        }
+      }
+      return new Date();
+    }
+  }
 } else {
   let cookiesData = $.getdata('CookiesJD') || "[]";
   cookiesData = jsonParse(cookiesData);
@@ -39,6 +70,7 @@ if ($.isNode()) {
   cookiesArr.reverse();
   cookiesArr.push(...[$.getdata('CookieJD2'), $.getdata('CookieJD')]);
   cookiesArr.reverse();
+  cookiesArr = cookiesArr.filter(item => item !== "" && item !== null && item !== undefined);
 }
 const JD_API_HOST = 'https://api.m.jd.com/api';
 !(async () => {
@@ -46,8 +78,24 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/', {"open-url": "https://bean.m.jd.com/"});
     return;
   }
+  $.log(`=====远程红包雨信息=====`)
   await getRedRain();
 	if(!$.activityId) return
+  let nowTs = new Date().getTime()
+  if (!($.st <= nowTs && nowTs < $.ed)) {
+    $.log(`远程红包雨配置获取错误，从本地读取配置`)
+    $.log(`\n`)
+    let hour = new Date().getUTCHours() + 8
+    if (ids[hour]){
+      $.activityId = ids[hour]
+      $.log(`本地红包雨配置获取成功`)
+    } else{
+      $.log(`无法从本地读取配置，请检查运行时间`)
+      return
+    }
+  } else{
+    $.log(`远程红包雨配置获取成功`)
+  }
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       cookie = cookiesArr[i];
@@ -55,7 +103,7 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
       $.index = i + 1;
       $.isLogin = true;
       $.nickName = '';
-      message = '';
+      message = `【${new Date($.st).getHours()}点${$.name}】`
       await TotalBean();
       console.log(`\n******开始【京东账号${$.index}】${$.nickName || $.UserName}*********\n`);
       if (!$.isLogin) {
@@ -63,19 +111,10 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
 
         if ($.isNode()) {
           await notify.sendNotify(`${$.name}cookie已失效 - ${$.UserName}`, `京东账号${$.index} ${$.UserName}\n请重新登录获取cookie`);
-        } else {
-          $.setdata('', `CookieJD${i ? i + 1 : ""}`);//cookie失效，故清空cookie。$.setdata('', `CookieJD${i ? i + 1 : "" }`);//cookie失效，故清空cookie。
         }
         continue
       }
-      let nowTs = new Date().getTime() + new Date().getTimezoneOffset() * 60 * 1000 + 8 * 60 * 60 * 1000
-      // console.log(nowTs, $.startTime, $.endTime)
-      if ($.startTime <= nowTs && nowTs < $.endTime) {
-        await receiveRedRain();
-      } else {
-        console.log(`不在红包雨时间之内`)
-        message += `不在红包雨时间之内`
-      }
+      await receiveRedRain();
       await showMsg();
     }
   }
@@ -87,7 +126,10 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
     $.done();
   })
 
-function showMsg() {
+async function showMsg() {
+  if ($.isNode() && !jdNotify) {
+    await notify.sendNotify(`【京东账号${$.index}】${$.nickName}`, message)
+  }
   return new Promise(resolve => {
     $.msg($.name, '', `【京东账号${$.index}】${$.nickName}\n${message}`);
     resolve()
@@ -97,10 +139,8 @@ function showMsg() {
 function getRedRain() {
   return new Promise(resolve => {
     $.get({
-      url: "https://gitee.com/shylocks/updateTeam/raw/main/jd_live_redRain.json",
-      headers:{
-        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
-      }}, (err, resp, data) => {
+      url: "http://ql4kk90rw.hb-bkt.clouddn.com/jd_live_redRain.json?" + Date.now(),
+      }, (err, resp, data) => {
       try {
         if (err) {
           console.log(`1111${JSON.stringify(err)}`)
@@ -109,8 +149,8 @@ function getRedRain() {
           if (safeGet(data)) {
             data = JSON.parse(data);
             $.activityId = data.activityId
-            $.startTime = data.startTime
-            $.endTime = data.endTime
+            $.st = data.startTime
+            $.ed = data.endTime
             console.log(`下一场红包雨开始时间：${new Date(data.startTime)}`)
             console.log(`下一场红包雨结束时间：${new Date(data.endTime)}`)
           }
@@ -138,11 +178,11 @@ function receiveRedRain() {
             if (data.subCode === '0') {
               console.log(`领取成功，获得${JSON.stringify(data.lotteryResult)}`)
               // message+= `领取成功，获得${JSON.stringify(data.lotteryResult)}\n`
-              message += `${data.lotteryResult.jPeasList[0].ext}:${(data.lotteryResult.jPeasList[0].quantity)}京豆\n`
+              message += `领取成功，获得 ${(data.lotteryResult.jPeasList[0].quantity)} 京豆\n`
 
             } else if (data.subCode === '8') {
-              console.log(`今日次数已满`)
-              message += `领取失败，今日已签到\n`;
+              console.log(`领取失败，已领过`)
+              message += `领取失败，已领过\n`;
             } else {
               console.log(`异常：${JSON.stringify(data)}`)
             }
@@ -159,7 +199,7 @@ function receiveRedRain() {
 
 function taskUrl(function_id, body = {}) {
   return {
-    url: `${JD_API_HOST}?functionId=${function_id}&body=${escape(JSON.stringify(body))}&client=wh5&clientVersion=1.0.0&_=${new Date().getTime() + new Date().getTimezoneOffset() * 60 * 1000 + 8 * 60 * 60 * 1000}`,
+    url: `${JD_API_HOST}?functionId=${function_id}&body=${escape(JSON.stringify(body))}&client=wh5&clientVersion=1.0.0&_=${new Date().getTime()}`,
     headers: {
       "Accept": "*/*",
       "Accept-Encoding": "gzip, deflate, br",
@@ -232,7 +272,7 @@ function jsonParse(str) {
       return JSON.parse(str);
     } catch (e) {
       console.log(e);
-      $.msg($.name, '', '不要在BoxJS手动复制粘贴修改cookie')
+      $.msg($.name, '', '请勿随意在BoxJs输入框修改内容\n建议通过脚本去获取cookie')
       return [];
     }
   }
