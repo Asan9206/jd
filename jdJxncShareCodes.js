535b4d11fe5e4a04becebfec6bb508fa4e0c7e3b
Asan9206 <60429656+Asan9206@users.noreply.github.com>
2021-01-30 19:57:11 +0800

    Add files via upload

--- jdJxncShareCodes.js
+++ jdJxncShareCodes.js
@@ -5,23 +5,27 @@
 支持京东N个账号
  */
 //云服务器腾讯云函数等NOde.js用户在此处填写京京喜农场的好友码。
-// github action用户的好友助力码填写到Action->Settings->Secrets->new Secret里面(Name填写 JxncShareCodes(此处的Name必须按此来写,不能随意更改),内容处填写助力码,填写规则如下)
 // 同一个京东账号的好友助力码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
+// 注意：京喜农场 种植种子发生变化的时候，互助码也会变！！
+// 注意：京喜农场 种植种子发生变化的时候，互助码也会变！！
+// 注意：京喜农场 种植种子发生变化的时候，互助码也会变！！
+// 每个账号 shareCdoe 是一个 json，示例如下
+// {"smp":"22bdadsfaadsfadse8a","active":"jdnc_1_btorange210113_2","joinnum":"1"}
 let JxncShareCodes = [
   '',//账号一的好友shareCode,不同好友中间用@符号隔开
   '',//账号二的好友shareCode，不同好友中间用@符号隔开
 ]
 // 判断github action里面是否有京喜农场助力码
-if (process.env.JXNCSHARECODES) {
-  if (process.env.JXNCSHARECODES.indexOf('&') > -1) {
+if (process.env.JXNC_SHARECODES) {
+  if (process.env.JXNC_SHARECODES.indexOf('&') > -1) {
     console.log(`您的京喜农场助力码选择的是用&隔开\n`)
-    JxncShareCodes = process.env.JXNCSHARECODES.split('&');
-  } else if (process.env.JXNCSHARECODES.indexOf('\n') > -1) {
+    JxncShareCodes = process.env.JXNC_SHARECODES.split('&');
+  } else if (process.env.JXNC_SHARECODES.indexOf('\n') > -1) {
     console.log(`您的京喜农场助力码选择的是用换行隔开\n`)
-    JxncShareCodes = process.env.JXNCSHARECODES.split('\n');
+    JxncShareCodes = process.env.JXNC_SHARECODES.split('\n');
   } else {
-    JxncShareCodes = process.env.JXNCSHARECODES.split();
+    JxncShareCodes = process.env.JXNC_SHARECODES.split();
   }
 } else if (process.env.JD_COOKIE) {
   // console.log(`由于您secret里面未提供助力码，故此处运行将会给脚本内置的码进行助力，请知晓！`)
